dd2c14c2e41cc28f02133f15c7aa7c0a0ea3754f
Robbie Langer <robbie0630dev@gmail.com>
2018-08-04 15:46:48 -0400

    changed preloader to use portal

--- src/components/Preloader.js
+++ src/components/Preloader.js
@@ -1,51 +1,11 @@
 import React from 'react';
+import { createPortal } from 'react-dom';
 
-class Preloader extends React.Component {
-  constructor(props) {
-    super(props);
-    this.elements = {};
-  }
-
-  componentDidMount() {
-    for (let href in this.props.content) {
-      let el = document.createElement('link');
-      el.rel = 'preload';
-      el.href = href;
-      el.as = this.props.content[href];
-      this.elements[href] = el;
-      document.head.appendChild(el);
-    }
-  }
-
-  componentDidUpdate(prevProps) {
-    for (let x in prevProps.content) {
-      if (!(x in this.props.content)) {
-        document.head.removeChild(this.elements[x]);
-        delete this.elements[x];
-      }
-    }
-    for (let href in this.props.content) {
-      if (!(href in prevProps.content)) {
-        let el = document.createElement('link');
-        el.rel = 'preload';
-        el.href = href;
-        el.as = this.props.content[href];
-        this.elements[href] = el;
-        document.head.appendChild(el);
-      }
-    }
-  }
-
-  componentWillUnmount() {
-    for (let href in this.elements) {
-      document.head.removeChild(this.elements[href]);
-      delete this.elements[href];
-    }
-  }
-
-  render() {
-    return null;
-  }
-}
+const Preloader = ({ content }) => createPortal(
+  Object.entries(content).map(([ href, as ]) => (
+    <link rel="preload" href={href} as={as} />
+  )),
+  document.head
+);
 
 export default Preloader;
