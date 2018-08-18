2c823d8f1de59087820d70465d2c65732b1ee70b
Robbie Langer <robbie0630dev@gmail.com>
2018-08-18 17:57:43 +0000

    prefetch instead of preload

--- src/components/Preloader.js
+++ src/components/Preloader.js
@@ -1,9 +1,12 @@
+// this component's name is a misnomer because it actually *prefetches* instead of preloading
+// it's name will be refactored soon
+
 import React from 'react';
 import { createPortal } from 'react-dom';
 
 const Preloader = ({ content }) => createPortal(
   Object.entries(content).map(([ href, as ]) => (
-    <link rel="preload" key={href} href={href} as={as} />
+    <link rel="prefetch" key={href} href={href} as={as} />
   )),
   document.head
 );
