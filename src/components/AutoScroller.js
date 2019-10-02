544292a5ff2a2d5252338429e63a9b4cccb44260
Robbie Langer <robbie0630dev@gmail.com>
2019-10-01 21:21:29 -0400

    ball

--- src/components/AutoScroller.js
+++ src/components/AutoScroller.js
@@ -1,5 +1,4 @@
 import React, { useRef, useEffect, useState } from "react"
-import { findDOMNode } from "react-dom"
 
 const getDisplayName = function getDisplayName(WrappedComponent) {
   return WrappedComponent.displayName || WrappedComponent.name || "Component"
